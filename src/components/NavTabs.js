0321d0efef1bee1112ca99d7c1472f74662c9966
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-11-11 11:06:12 -0500

    worked om navbar css

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -5,7 +5,7 @@ import "./NavTabsStyles.css"
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav m-4 justify-content-center" >
+    <ul className="nav nav-tabs m-3 p-2 justify-content-center" >
       <li className="nav-item">
         <a
           href="/"
